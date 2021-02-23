d6e68f1b71f17c73a23faa0f43287fcbcea24647
Miro <MiroPhoenix91@gmail.com>
2021-02-23 22:09:48 +0100

    vue3 testing

--- app.js
+++ app.js
@@ -2,14 +2,26 @@ const app = Vue.createApp({
     data() {
         return {
             showBooks: true,
-            title: 'The final Empire',
-            author: 'Amir Tarig',
-            age: 30
+            books: [
+                {title: 'name of the wind', author: 'patrick rothfuss'},
+                {title: 'the way of kings', author: 'brandon sanderson'},
+                {title: 'the final empire', author: 'brandon sanderson'}
+            ],
+            age: 30,
+            x: 0,
+            y: 0
         }
     },
     methods: {
         toggleBooks() {
             this.showBooks = !this.showBooks
+        },
+        handleEvent(){
+            console.log(event)
+        },
+        handleMousemove(e){
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 }).mount('#app')
