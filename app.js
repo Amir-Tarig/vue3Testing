98f11c36d19006c317f259b6495cef608010ee90
Miro <MiroPhoenix91@gmail.com>
2021-02-23 22:54:24 +0100

    vue 3 first look

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const app = Vue.createApp({
     data() {
         return {
+            url: 'https://github.com/MiroPhoenix',
             showBooks: true,
             books: [
-                {title: 'name of the wind', author: 'patrick rothfuss'},
-                {title: 'the way of kings', author: 'brandon sanderson'},
-                {title: 'the final empire', author: 'brandon sanderson'}
+                {title: 'name of the wind', author: 'patrick rothfuss',img:'./assets/1.jpg', isFav: true},
+                {title: 'the way of kings', author: 'brandon sanderson', img:'./assets/2.jpg',isFav: false},
+                {title: 'the final empire', author: 'brandon sanderson',img:'./assets/3.jpg',isFav: true}
             ],
             age: 30,
             x: 0,
@@ -13,6 +14,9 @@ const app = Vue.createApp({
         }
     },
     methods: {
+        toggleClass(book){
+            book.isFav = !book.isFav
+        },
         toggleBooks() {
             this.showBooks = !this.showBooks
         },
@@ -22,6 +26,11 @@ const app = Vue.createApp({
         handleMousemove(e){
             this.x = e.offsetX
             this.y = e.offsetY
+        }
+    },
+    computed: {
+        filterBooks() {
+            return this.books.filter(book => book.isFav)
         }
     }
 }).mount('#app')
